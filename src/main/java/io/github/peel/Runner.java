dbf6de2020f8dd9a7919a5455cdfd6c7d6347bd1
peel <plimanowski@gmail.com>
2014-03-02 21:04:43 +0100

    messaging in java - idea

---
+++ src/main/java/io/github/peel/Runner.java
@@ -0,0 +1,29 @@
+package io.github.peel;
+
+import com.sun.jersey.spi.container.servlet.ServletContainer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Runner {
+    public static void main(String[] args){
+        Server server = new Server(1234);
+        ServletContextHandler ctx = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+        ctx.setContextPath("/");
+        server.setHandler(ctx);
+
+        ServletHolder jerseyServlet = ctx.addServlet(ServletContainer.class, "/*");
+        jerseyServlet.setInitOrder(1);
+        jerseyServlet.setInitParameter("com.sun.jersey.config.property.packages","io.github.peel");
+
+        try {
+            server.start();
+            server.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
